ce250968f48e805b97fa88a30dfda7413e8ef6ce
Eugene <varlamoved@gmail.com>
2021-10-20 06:54:27 +0300

    use more natural way to add Helpers

--- lib/hermitage/railtie.rb
+++ lib/hermitage/railtie.rb
@@ -5,7 +5,9 @@ require 'hermitage/view_helpers'
 module Hermitage
   class Railtie < Rails::Railtie
     initializer 'hermitage.view_helpers' do |_app|
-      ActionView::Base.send :include, ViewHelpers
+      ActiveSupport.on_load :action_view do
+        ActionView::Base.send :include, ViewHelpers
+      end
     end
   end
 end
